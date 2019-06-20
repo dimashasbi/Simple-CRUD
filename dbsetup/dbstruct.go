8d5b140f7d503373362e0e8d18ff648339587545
dimashasbi <dimas.habibi@artajasa.co.id>
2019-06-20 18:38:41 +0700

    Update System Settings

--- dbsetup/dbstruct.go
+++ dbsetup/dbstruct.go
@@ -32,65 +32,90 @@ type CaAPIMessages struct {
 
 // CaAPITransactions for Logging Payment Transaction of API
 type CaAPITransactions struct {
-	ID                       uint `gorm:"primary_key"`
-	DateTime                 time.Time
-	RequestID                string
-	AuthKey                  string
-	TransactionType          int
-	ProductIndicator         string
-	RequestData              string
-	ResponseData             string
-	CustomerNumber           string
-	TerminalID               string
-	SystemTraceAuditNumber   string
-	RetrievalReferenceNumber string
-	ResponseCode             string
-	Status                   string
-	Reversed                 string
-	ResponseDescription      string
-	OriginalDataElement      string
-	AcquiringInstitutionID   string
+	ID                     uint `gorm:"primary_key"`
+	DateTime               time.Time
+	RequestID              string
+	AuthKey                string
+	TransactionType        int
+	ProductIndicator       string
+	RequestData            string
+	ResponseData           string
+	CustomerNumber         string
+	ResponseCode           string
+	Status                 string
+	Reversed               string
+	TerminalID             string
+	STAN                   string
+	RRN                    string
+	ResponseDescription    string
+	TransmissionDateTime   time.Time
+	TransactionDate        time.Time
+	OriginalDataElement    string
+	AcquiringInstitutionID string
+	IsoMessages            string
 }
 
-// CaSvaTransactions for Logging SVA Transaction of API
-type CaSvaTransactions struct {
-}
+// // CaSvaTransactions for Logging SVA Transaction of API
+// type CaSvaTransactions struct {
+// }
 
-// CaTransactions for Logging CaAPITransactions but full ISO messages
-type CaTransactions struct {
+// // CaTransactions for Logging CaAPITransactions but full ISO messages
+// type CaTransactions struct {
+// 	ID                       uint `gorm:"primary_key"`
+// 	DateTime                 time.Time
+// 	RequestID                string
+// 	MTI                      string
+// 	PrimaryAccountNumber     string
+// 	ProcessingCode           string
+// 	TransactionType          int
+// 	Amount                   int
+// 	TransmissionDateTime     time.Time
+// 	SystemTraceAuditNumber   string
+// 	TransactionDateTime      time.Time
+// 	LocalTransactionDate     string
+// 	LocalTransactionTime     string
+// 	MerchantType             string
+// 	AcquiringInstitutionID   string
+// 	ForwardInstitutionID     string
+// 	RetrievalReferenceNumber string
+// 	CardAcceptorTerminalID   string
+// 	CardAcceptorID           string
+// 	CurrencyCode             string
+// 	ResponseCode             string
+// 	FromAccountNumber        string
+// 	TransactionIndicator     string
+// 	ProductIndicator         string
+// 	CustomerNumber           string
+// 	OriginalDataElement      string
+// 	SessionID                string
+// 	Status                   string
+// 	Reversed                 string
+// 	AdditionalDataReq        string
+// 	AdditionalDataResp       string
+// }
+
+// IsoAPIMessages for Logging Transaction between API and system behind
+type IsoAPIMessages struct {
 	ID                       uint `gorm:"primary_key"`
+	DirectionCode            int
 	DateTime                 time.Time
-	RequestID                string
 	MTI                      string
 	PrimaryAccountNumber     string
 	ProcessingCode           string
-	TransactionType          int
-	Amount                   int
+	TransactionAmount        int
 	TransmissionDateTime     time.Time
 	SystemTraceAuditNumber   string
-	TransactionDateTime      time.Time
 	LocalTransactionDate     string
 	LocalTransactionTime     string
-	MerchantType             string
 	AcquiringInstitutionID   string
 	ForwardInstitutionID     string
 	RetrievalReferenceNumber string
 	CardAcceptorTerminalID   string
 	CardAcceptorID           string
+	AdditionalData           string
 	CurrencyCode             string
 	ResponseCode             string
 	FromAccountNumber        string
 	TransactionIndicator     string
-	ProductIndicator         string
-	CustomerNumber           string
 	OriginalDataElement      string
-	SessionID                string
-	Status                   string
-	Reversed                 string
-	AdditionalDataReq        string
-	AdditionalDataResp       string
-}
-
-// IsoAPIMessages for Logging Transaction between API and system behind
-type IsoAPIMessages struct {
 }
