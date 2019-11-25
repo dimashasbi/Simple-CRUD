0aab94cd33b78bc06f4cc358a86975e88abe04b5
dimashasbi <dimas.habibi@artajasa.co.id>
2019-11-25 15:54:55 +0700

    Create Test

--- app/main_test.go
+++ app/main_test.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"M-GateDBConfig/adapter"
-	"M-GateDBConfig/app/fileconfig"
 	"M-GateDBConfig/engine"
 	"M-GateDBConfig/model"
+	"M-GateDBConfig/provider/fileconfig"
 	"M-GateDBConfig/provider/postgres"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/jinzhu/gorm"
 	"github.com/spf13/viper"
@@ -17,10 +19,18 @@ type (
 	TestHandler struct {
 		dbHandler *gorm.DB
 	}
+
+	TestFactory struct {
+		dbconf  model.DBConfigurationModel
+		eng     engine.EnginesFactory
+		dbhand  engine.StorageFactory
+		adapter adapter.Handler
+	}
 )
 
+// real value
 var (
-	testDBValue = model.DBConfigurationModel{
+	DBConf = model.DBConfigurationModel{
 		Dbname:   "hasbidatabase",
 		Password: "dimskii",
 		Host:     "127.0.0.1",
@@ -29,6 +39,7 @@ var (
 	}
 )
 
+// TestGetDBConfig test getting file config
 func TestGetDBConfig(T *testing.T) {
 	// Test if value JSON is not same
 
@@ -38,62 +49,91 @@ func TestGetDBConfig(T *testing.T) {
 	// test - get fileconfig
 	db := fileconfig.GetDBConfig()
 
-	checkdbconfvalue(testDBValue, db, T)
+	checkdbconfvalue(DBConf, db, T)
 
 	// return real value for Development
-	returnRealDBconfValue()
+	fileconfig.CreateDBConfig()
 
 }
 
-func InitializeApps() {
-	// Load Configuration
-	dbConfig := fileconfig.GetDBConfig()
-	// Connect and Migrate DB
-	db := postgres.NewStorage(dbConfig)
-	// Prepare Engine for Use Case Logic
-	eng := engine.NewEngine(db)
-	// Start application
-	adapter := adapter.Handler{}
-	adapter.InitializeServer(eng)
-	// adapter.Run(":4911")
-}
-
+// TestNewStorage for checking Table in DB exist or not.
 func TestNewStorage(T *testing.T) {
 	// 1. Test Table Existing or Not after Migration
 
 	// DB Migration first
-	postgres.NewStorage(testDBValue)
+	postgres.NewStorage(DBConf)
 
 	// Test Table Existing or Not
 	// connect DB
-	data := testDBValue
-	db := InitDB(data)
+	data := DBConf
+	db := initDB(data)
 	// checkTable exist or not
-	result := db.Exec("SELECT * FROM REGRISTRIES")
+	result := db.Exec("SELECT * FROM REGISTRIES")
+	if result.Error != nil {
+		T.Errorf("No Table Exists %+v\n", result.Error)
+	}
+	result = db.Exec("SELECT * FROM PARAMETERS")
 	if result.Error != nil {
 		T.Errorf("No Table Exists %+v\n", result.Error)
 	}
 }
 
-func TestInsertParameterToDB(T *testing.T) {
+// TestTableParameter for checking Table in DB exist or not.
+func TestTableParameter(T *testing.T) {
+	// Test Insert Parameter
+	var ParameterEngine engine.Parameter
+	tf := &TestFactory{}
+	tf.initializeApps()
+	ParameterEngine = tf.eng.NewParameter()
+
+	// Expected
+	expected := model.Parameters{
+		Key:   "haha",
+		Value: "meeong",
+	}
+
+	// input value to the Case
+	inp := engine.AddParamReq{
+		ID:    "0",
+		Key:   &expected.Key,
+		Value: &expected.Value,
+	}
+
+	// Do the Test
+	ParameterEngine.Add(&inp)
+
+	// Actual
+	actual := model.Parameters{}
+	dbase := initDB(tf.dbconf)
+	result := dbase.Find(&actual)
+	if result.Error != nil {
+		T.Errorf("Error Find %+v\n", result.Error)
+	}
+
+	assert.Equal(T, expected.Key, actual.Key)
+	assert.Equal(T, expected.Value, actual.Value)
+
+	// clean Test from DB
+	dbase.Where("key = ?", expected.Key).Delete(&expected)
+
+	// other test plan
+	// 1. Test unique KEY
+	// 2. Test no Table
+	// 3. Test no Connection of Database
+
+}
+
+func (t *TestFactory) initializeApps() {
+
 	// Initialize Application First
-	InitializeApps()
-
-	// Test Insert Parameter to Database
-
-	// Test unique KEY
-
-	// Test no Table
-
-	// Test no Connection of Database
-
+	t.dbconf = fileconfig.GetDBConfig()
+	// Connect and Migrate DB
+	t.dbhand = postgres.NewStorage(t.dbconf)
+	// Prepare Engine for Use Case Logic
+	t.eng = engine.NewEngine(t.dbhand)
 }
 
-func returnRealDBconfValue() {
-	fileconfig.CreateDBConfig()
-}
-
-func EditDBconfValue(nan model.DBConfigurationModel) {
+func editDBconfValue(nan model.DBConfigurationModel) {
 	v := viper.New()
 	v.SetConfigType("json")
 	v.AddConfigPath("./")
@@ -107,8 +147,7 @@ func checkdbconfvalue(expected, actual model.DBConfigurationModel, t *testing.T)
 	}
 }
 
-
-func InitDB(data model.DBConfigurationModel) *gorm.DB {
+func initDB(data model.DBConfigurationModel) *gorm.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		data.Host, data.Port, data.User, data.Password, data.Dbname)
